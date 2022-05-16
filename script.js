c0c6d0e934184190a32b41bba51f09515b876502
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-16 15:47:37 +0530

    starting condition added

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 // Selecting elements
 const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("#score--1");
+const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
