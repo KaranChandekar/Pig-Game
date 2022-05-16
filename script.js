e74d01985f4946683b76aeb8fa9258ba36b6bed4
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-16 16:55:45 +0530

    add dice to current score

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -13,6 +15,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   // 1. Generating a random dice roll
@@ -21,4 +25,13 @@ btnRoll.addEventListener("click", function () {
   // 2. Display dice
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-images/dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // Change later
+  } else {
+    // Switch to next player
+  }
 });
