3945ec46132861e24dd976dc19f3a6c057ad3743
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-16 15:42:15 +0530

    select elements

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("#score--1");
+const diceEl = document.querySelector(".dice");
