c5609716e909308cddb5473616cd1ab4d3136093
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-16 16:29:11 +0530

    selecting buttons

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
 score0El.textContent = 0;
