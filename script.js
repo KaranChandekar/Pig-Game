0a2acf58f2db49d0dfa55a0bec1d5c256bc21148
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-17 19:21:11 +0530

    toggle class

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -38,5 +40,7 @@ btnRoll.addEventListener("click", function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
