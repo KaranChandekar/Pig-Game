0c129342d20742a5564ae2ec9e2a3e7b798b5417
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-17 19:16:20 +0530

    switch to next player

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -30,8 +32,11 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // Change later
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
